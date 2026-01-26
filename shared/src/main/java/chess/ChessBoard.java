f4b387a6c0fea9d9456f3cf505a0b4b8dd3656b1
Briannon Woolsey <woolsey0@byu.edu>
2026-01-26 00:35:09 -0700

    ChessBoard Mostly Complete

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -8,8 +11,24 @@ package chess;
  */
 public class ChessBoard {
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+        return Objects.deepEquals(board, that.board);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(board);
+    }
+
+    private ChessPiece[][] board;
+
     public ChessBoard() {
-        
+        board = new ChessPiece[8][8];
     }
 
     /**
@@ -19,7 +38,9 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        int row = position.getRow() -1;
+        int col = position.getColumn() - 1;
+        board[row][col] = piece;
     }
 
     /**
@@ -30,7 +51,9 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        int row = position.getRow() -1;
+        int col = position.getColumn() - 1;
+        return board[row][col];
     }
 
     /**
@@ -38,6 +61,32 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        board = new ChessPiece[8][8];
+
+        for (int col = 1; col <= 8; col++) {
+            addPiece(new ChessPosition(2, col), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN));
+            addPiece(new ChessPosition(7, col), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN));
+        }
+
+//        int[] direction = {1, -1};
+//        int[] fbrow = {1, 8};
+//        int[] column = {1, 8};
+//        ChessGame.TeamColor[] colors = {
+//                ChessGame.TeamColor.WHITE,
+//                ChessGame.TeamColor.BLACK
+//        };
+//        for (int d = 0; d < 2; d++) {
+//            int col = column[d];
+//            addPiece(new ChessPosition(fbrow[d], col), new ChessPiece(colors[d], ChessPiece.PieceType.ROOK));
+//            addPiece(new ChessPosition(fbrow[d], col), new ChessPiece(colors[d], ChessPiece.PieceType.KNIGHT));
+//            addPiece(new ChessPosition(fbrow[d], col), new ChessPiece(colors[d], ChessPiece.PieceType.BISHOP));
+//            addPiece(new ChessPosition(fbrow[d], col), new ChessPiece(colors[d], ChessPiece.PieceType.QUEEN));
+//            addPiece(new ChessPosition(fbrow[d], col), new ChessPiece(colors[d], ChessPiece.PieceType.KING));
+//            addPiece(new ChessPosition(fbrow[d], col), new ChessPiece(colors[d], ChessPiece.PieceType.BISHOP));
+//            addPiece(new ChessPosition(fbrow[d], col), new ChessPiece(colors[d], ChessPiece.PieceType.KNIGHT));
+//            addPiece(new ChessPosition(fbrow[d], col), new ChessPiece(colors[d], ChessPiece.PieceType.ROOK));
+//            col += direction[d];
+//        }
+
     }
 }
