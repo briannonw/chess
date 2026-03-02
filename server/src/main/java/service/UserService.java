f306894414eb278d10875f0019d803c167fd5e54
Briannon Woolsey <woolsey0@byu.edu>
2026-03-01 22:05:56 -0700

    RegisterRequest Record Complete

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,4 +1,27 @@
 package service;
 
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import model.*;
+
 public class UserService {
+    private final DataAccess dataAccess;
+
+    public UserService(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+
+    public RegisterResult register(RegisterRequest registerRequest) throws DataAccessException {
+        UserData user = new UserData(registerRequest.username(), registerRequest.password(), registerRequest.email());
+        dataAccess.createUser(user);
+
+    }
+
+    public LoginResult login(LoginRequest loginRequest) {
+
+    }
+
+    public void logout(LogoutRequest logoutRequest) {
+
+    }
 }
