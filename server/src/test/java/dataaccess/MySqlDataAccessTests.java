080f28fe46d29ae3befee8d9ab0cf4ac4f242a8f
Briannon Woolsey <woolsey0@byu.edu>
2026-03-13 23:18:34 -0600

    Game tests finished

--- server/src/test/java/dataaccess/MySqlDataAccessTests.java
+++ server/src/test/java/dataaccess/MySqlDataAccessTests.java
@@ -114,22 +114,98 @@ public class MySqlDataAccessTests {
         assertNull(result);
     }
 
-    // createGame pass
+    @Test
+    @DisplayName("Valid Creation")
+    public void createGameSuccess() throws DataAccessException {
+        GameData game = new GameData(1, null, null, "TestGame", new ChessGame());
 
-    // createGame fail
+        dataAccess.createGame(game);
 
-    // getGame pass
+        GameData result = dataAccess.getGame(1);
 
-    // getGame fail
+        assertNotNull(result);
+        assertEquals(1, result.gameID());
+        assertEquals("TestGame", result.gameName());
+    }
 
-    // listGames pass
+    @Test
+    @DisplayName("Create Game Bad Request")
+    public void createGameBadRequest() {
+        GameData game = new GameData(1, null, null, null, new ChessGame());
 
-    // listGames fail
+        assertThrows(DataAccessException.class, () -> dataAccess.createGame(game));
+    }
 
-    // updateGame pass
+    @Test
+    @DisplayName("Get Game Success")
+    public void getGameSuccess() throws DataAccessException {
+        GameData game = new GameData(1, null, null, "Test Game", new ChessGame());
+        dataAccess.createGame(game);
 
-    // updateGame fail
+        GameData result = dataAccess.getGame(1);
 
+        assertNotNull(result);
+        assertEquals(1, result.gameID());
+        assertEquals("Test Game", result.gameName());
+    }
+
+    @Test
+    @DisplayName("Get Game Fail")
+    public void getGameFail() throws DataAccessException {
+        GameData result = dataAccess.getGame(-1);
+
+        assertNull(result);
+    }
+
+    @Test
+    @DisplayName("List Games Success")
+    public void listGamesSuccess() throws DataAccessException {
+        GameData game1 = new GameData(1, null, null, "Test Game 1", new ChessGame());
+        dataAccess.createGame(game1);
+
+        GameData game2 = new GameData(2, null, null, "Test Game 2", new ChessGame());
+        dataAccess.createGame(game2);
+
+        var games = dataAccess.listGames();
+
+        assertEquals(2, games.size());
+    }
+
+    @Test
+    @DisplayName("List Games Empty")
+    public void listGamesFail() throws DataAccessException {
+        var games = dataAccess.listGames();
+
+        assertEquals(0, games.size());
+    }
+
+    @Test
+    @DisplayName("Update Game Success")
+    public void updateGameSuccess() throws DataAccessException {
+        GameData game = new GameData(1, null, null, "Test Game", new ChessGame());
+        dataAccess.createGame(game);
+
+        GameData updatedGame = new GameData(1, "White", null, "Updated Game", game.game());
+        dataAccess.updateGame(updatedGame);
+
+        GameData result = dataAccess.getGame(1);
+        assertNotNull(result);
+        assertEquals("Updated Game", result.gameName());
+        assertEquals("White", result.whiteUsername());
+        assertNull(result.blackUsername());
+    }
+
+    @Test
+    @DisplayName("Update Game Fail")
+    public void updateGameFail() throws DataAccessException {
+        GameData game = new GameData(-1, null, null, "Test Game", new ChessGame());
+
+        dataAccess.updateGame(game);
+
+        GameData result = dataAccess.getGame(-1);
+        assertNull(result);
+    }
+    
     // createAuth pass
 
     // createAuth fail
